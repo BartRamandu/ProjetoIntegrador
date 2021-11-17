0a626ebd7fe1bbbd07d64de7f47c58d57713a6ae
pedrosantosara <pedrohensantosaraujo@gmail.com>
2021-11-17 12:12:30 -0300

    @pedrosantosara[Task: Implement Spring Security]

---
+++ Java/src/main/java/com/projetointegrador/cultivar/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.projetointegrador.cultivar.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projetointegrador.cultivar.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	public Optional<Usuario> findByUsuario(String usuario);
+
+}
